66a74e2826a6b07c28d1453e95aa1c42be35b66a
Jean Martelot <jeanmartelot@gmail.com>
2017-08-03 13:51:57 +0200

    Check if poeditor branch exist

--- scripts/poeditor.sh
+++ scripts/poeditor.sh
@@ -3,7 +3,13 @@
 #Import new terms from github into poeditor
 curl "https://poeditor.com/api/webhooks/github?api_token=be16af5e4c263251da66a8efcabb1182&id_project=122575&language=en&operation=import_terms_and_translations"
 
-POEDITOR_BRANCH='poeditor'
+POEDITOR_BRANCH='test'
+
+if [ ! `git branch --list $POEDITOR_BRANCH` ]
+then
+    echo "Create $POEDITOR_BRANCH branch"
+    git branch $POEDITOR_BRANCH
+fi
 
 #Switch branch
 git checkout $POEDITOR_BRANCH
