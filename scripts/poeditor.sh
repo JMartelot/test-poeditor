46c44aaff4d11a90fe62df28659b4ef64e55289a
Jean Martelot <jeanmartelot@gmail.com>
2017-08-03 10:44:13 +0200

    Update poeditor.sh

--- scripts/poeditor.sh
+++ scripts/poeditor.sh
@@ -4,4 +4,4 @@
 curl "https://poeditor.com/api/webhooks/github?api_token=be16af5e4c263251da66a8efcabb1182&id_project=122575&language=en&operation=import_terms_and_translations"
 
 #Update translations files for other languages than default one (English)
-curl "https://poeditor.com/api/webhooks/github?api_token=be16af5e4c263251da66a8efcabb1182&id_project=122575&language=fr&operation=export_terms_and_translations"
+#curl "https://poeditor.com/api/webhooks/github?api_token=be16af5e4c263251da66a8efcabb1182&id_project=122575&language=fr&operation=export_terms_and_translations"
